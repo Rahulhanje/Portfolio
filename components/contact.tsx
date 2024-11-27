85c42421f745774d6f3fb62f953c1637d4772f10
Rahul <rahulhanje0.7@gmail.com>
2024-11-27 17:33:49 +0530

    Contact updated

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useRef } from "react";
+import React, { useRef } from "react";
 import SectionHeading from "./section-heading";
 import Link from "next/link";
 import { CiLocationOn } from "react-icons/ci";
@@ -10,7 +10,8 @@ import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { GrResume } from "react-icons/gr";
 import { useSectionInView } from "@/lib/hooks";
-
+import Player from "lottie-react";
+import contact from "../public/contact.json";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -20,7 +21,7 @@ export default function Contact() {
     <motion.section
       id="contact"
       ref={ref}
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="mb-20 sm:mb-28 w-full px-4 text-center"
       initial={{
         opacity: 0,
         y: 100,
@@ -34,57 +35,106 @@ export default function Contact() {
       }}
     >
       <SectionHeading>Contact me</SectionHeading>
-
-      <p className="text-gray-700 -mt-6 dark:text-white/80 py-5">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:rahulhanje0.7@gmail.com">
-          rahulhanje0.7@gmail.com
-        </a>{" "}
-      
-      </p>
-      
-            <motion.div
-                className="text-center"
-                whileInView={{ y: [-20, 0], opacity: [0, 1] }}
-                transition={{ duration: 0.75 }}
+      <p className="text-gray-700 dark:text-white/80 py-5 text-sm sm:text-base">
+            Please contact me directly at{" "}
+            <a
+              className="underline text-blue-500 hover:text-blue-700 transition"
+              href="mailto:rahulhanje0.7@gmail.com"
             >
-              
+              rahulhanje0.7@gmail.com
+            </a>
+          </p>
+      <div className="flex flex-col sm:flex-row justify-center items-center sm:gap-x-32 w-full">
+        {/* Lottie Animation */}
+        <div className="mb-8 sm:mb-0 flex justify-center">
+          <Player
+            autoplay
+            loop
+            animationData={contact}
+            className="w-[250px] h-[250px] sm:w-[350px] sm:h-[350px]"
+          />
+        </div>
 
-                <div className="flex flex-col lg:flex-row justify-center gap-8 lg:gap-16 text-gray-300 font-semibold">
-                    <div className="flex flex-col gap-3 md:gap-6 font-poppins">
-                        <ContactItem icon={<MdAlternateEmail className="text-orange-500 text-lg" />} label="rahulhanje0.7@gmail.com" />
-                        <ContactItem icon={<IoMdCall className="text-green-500 text-lg" />} label="+91-7204169306" />
-                        <ContactItem icon={<CiLocationOn className="text-red-700 text-lg" />} label="Belagavi, Karnataka" />
-                    </div>
-                    
-                    <div className="flex flex-col gap-3 md:gap-6 font-poppins">
-                        <LinkItem icon={<FaInstagram className="text-pink-500 text-lg" />} href="https://www.instagram.com/rahulhanje07/" label="rahulhanje07" />
-                        <LinkItem icon={<FaLinkedin className="text-blue-500 text-lg" />} href="https://www.linkedin.com/in/rahul-hanje-b31812252/" label="Rahul Hanje" />
-                        <LinkItem icon={<FaGithub color="white" className="text-black text-lg" />} href="https://github.com/Rahulhanje" label="Rahulhanje" />
-                        <div className="sm:hidden">
-                            <LinkItem icon={<GrResume color="white" className="text-black text-lg" />} href="https://drive.google.com/file/d/1cyQbmoy75Acfh3VlueSPGpKSCK_DHlIT/view?usp=sharing" label="Resume" />
-                        </div>
-                    </div>
-                </div> 
-                <p className="font-semibold pt-5"> Developed by ✨ @Rahul ✨</p>
-            </motion.div>
-       
-    
+        <div className="w-full max-w-lg">
+          
+
+          <motion.div
+            className="text-center"
+            whileInView={{ y: [-20, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.75 }}
+          >
+            <div className="flex flex-col md:flex-row justify-center gap-8 text-gray-300 font-semibold">
+              {/* Contact Items */}
+              <div className="flex flex-col gap-4">
+                <ContactItem
+                  icon={<MdAlternateEmail className="text-orange-500 text-lg" />}
+                  label="rahulhanje0.7@gmail.com"
+                />
+                <ContactItem
+                  icon={<IoMdCall className="text-green-500 text-lg" />}
+                  label="+91-7204169306"
+                />
+                <ContactItem
+                  icon={<CiLocationOn className="text-red-700 text-lg" />}
+                  label="Belagavi, Karnataka"
+                />
+              </div>
+
+              {/* Link Items */}
+              <div className="flex flex-col gap-4">
+                <LinkItem
+                  icon={<FaInstagram className="text-pink-500 text-lg" />}
+                  href="https://www.instagram.com/rahulhanje07/"
+                  label="rahulhanje07"
+                />
+                <LinkItem
+                  icon={<FaLinkedin className="text-blue-500 text-lg" />}
+                  href="https://www.linkedin.com/in/rahul-hanje-b31812252/"
+                  label="Rahul Hanje"
+                />
+                <LinkItem
+                  icon={<FaGithub className="text-black text-lg" />}
+                  href="https://github.com/Rahulhanje"
+                  label="Rahulhanje"
+                />
+                <LinkItem
+                  icon={<GrResume className="text-black text-lg" />}
+                  href="https://drive.google.com/file/d/1cyQbmoy75Acfh3VlueSPGpKSCK_DHlIT/view?usp=sharing"
+                  label="Resume"
+                />
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
     </motion.section>
   );
 }
+
 const ContactItem = ({ icon, label }: { icon: JSX.Element; label: string }) => (
   <div className="flex items-center justify-center bg-[#1a1a2e] rounded-lg p-3 hover:bg-[#23233d] shadow-md transform hover:scale-105 transition duration-300 ease-in-out">
-      {icon}
-      <p className="text-sm font-bold ml-2 text-center">{label}</p>
+    {icon}
+    <p className="text-sm font-bold ml-2 text-center">{label}</p>
   </div>
 );
 
-const LinkItem = ({ icon, href, label }: { icon: JSX.Element; href: string; label: string }) => (
+const LinkItem = ({
+  icon,
+  href,
+  label,
+}: {
+  icon: JSX.Element;
+  href: string;
+  label: string;
+}) => (
   <Link href={href} passHref legacyBehavior>
-      <a target="_blank" rel="noopener noreferrer" className="flex items-center justify-center bg-[#1a1a2e] rounded-lg p-3 hover:bg-[#23233d] shadow-md transform hover:scale-105 transition duration-300 ease-in-out">
-          {icon}
-          <p className="text-sm font-bold ml-2 text-center">{label}</p>
-      </a>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center justify-center bg-[#1a1a2e] rounded-lg p-3 hover:bg-[#23233d] shadow-md transform hover:scale-105 transition duration-300 ease-in-out"
+    >
+      {icon}
+      <p className="text-sm font-bold ml-2 text-center">{label}</p>
+    </a>
   </Link>
 );
