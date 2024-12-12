3c21ef76ca94dd19e9a44f4795ccff24b6bb42e1
Rahul <rahulhanje0.7@gmail.com>
2024-12-12 21:41:06 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,25 @@
-import About from "@/components/about";
-import AboutMe from "@/components/aboutme";
 
+import AboutMe from "@/components/aboutme";
 import Contact from "@/components/contact";
 import Education from "@/components/Education";
-
-import { GridBackground } from "@/components/intro";
-import {Opensource} from "@/components/Opensource";
+import { Opensource } from "@/components/Opensource";
 import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
-
-
 
 export default function Home() {
   return (
-
-    <main className="flex flex-col items-center px-4">
-      {/* <SectionDivider /> */}
-      {/* <GridBackground> */}
-      <div className="absolute top-12 left-12 w-96 h-96 bg-blue-800 rounded-full filter blur-[120px] opacity-40" />
-      <div className="absolute  right-0 w-96 h-96 bg-blue-800 rounded-full filter blur-[100px] opacity-40" />
-      {/* <div className="absolute top-20 right-20 w-72 h-72 bg-blue-400/30 dark:bg-none rounded-full filter blur-2xl" /> */}
-      <AboutMe/>
-      {/* </GridBackground> */}
-      <Education/>
+    <main className="flex flex-col items-center px-4 sm:px-2 md:px-6 lg:px-12">
+      {/* Background Circles */}
+      <div className="hidden sm:block">
+        <div className="absolute top-12 left-12 w-96 h-96 bg-blue-800 rounded-full filter blur-[120px] opacity-40 sm:top-6 sm:left-6 sm:w-64 sm:h-64 md:w-80 md:h-80" />
+        <div className="absolute right-0 w-96 h-96 bg-blue-800 rounded-full filter blur-[100px] opacity-40 sm:w-64 sm:h-64 md:w-80 md:h-80" />
+      </div>
+      {/* Components */}
+      <AboutMe />
+      <Education />
       <Skills />
-     
       <Projects />
-
-      <Opensource/>
-      {/* <Experience /> */}
+      <Opensource />
       <Contact />
     </main>
   );
